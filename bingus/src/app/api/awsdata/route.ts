bbc2c2fbed8cdbbe665fb5c0d48cae4b9ca1f6ef
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-09-03 23:43:39 -0700

    shivam's currect code for rich

--- bingus/src/app/api/awsdata/route.ts
+++ bingus/src/app/api/awsdata/route.ts
@@ -1,9 +1,24 @@
 import handleError from "@/lib/errors";
 import { NextRequest } from "next/server";
+import pg from "pg";
+const { Client } = pg;
 
 //get aws data here
 export async function GET(request: NextRequest): Promise<Response> {
     try {
+        const client = new Client({
+            password: "Bingus_LLC",
+            host: "bingus-postgres-1.criiocw0mlgp.us-east-2.rds.amazonaws.com",
+            post: 5432,
+            databse: "Bingus-DB"
+        })
+
+        console.log("___________BEFORE CONNECTING______________________")
+
+        await client.connect()
+
+        console.log("____________________AFTER_______________________________")
+
         return new Response();
     }
     catch (error) {
--- bingus/src/lib/actions.ts
+++ bingus/src/lib/actions.ts
@@ -2,6 +2,7 @@
 
 export async function fetchAwsData(prevState: any, formData: FormData) {
     const response = await fetch("http://localhost:3000/api/awsdata");
+    console.log("LOLOLOLO")
     const json = await response.json();
     return {
         data: json
