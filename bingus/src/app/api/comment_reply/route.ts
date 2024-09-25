0c868e2f9a5e1e4b353ca33f669e4b80f8538d78
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-09-25 11:09:21 -0700

    made crud api for comment_reply

--- bingus/src/app/api/comment_reply/route.ts
+++ bingus/src/app/api/comment_reply/route.ts
@@ -19,13 +19,80 @@ export async function GET(request: NextRequest) {
         const result = await client.query("select * from comment_reply");
         client.release();
 
-        return new Response(JSON.stringify(result.rows), {
-            status: 200,
+        return new Response(JSON.stringify(result.rows), { status: 200 });
+    } catch (error) {
+        return new Response("Failed to retrieve data", { status: 500 });
+    }
+}
+
+export async function POST(request:NextRequest) {
+    try {
+        const pool = new Pool({
+            user: "postgres",
+            password: "Bingus_LLC",
+            host: "bingus-db-1.c9ayqsiuu3wz.us-east-1.rds.amazonaws.com",
+            port: 5432,
+            database: "bingus",
+            ssl: {
+                rejectUnauthorized: false,
+            },
         });
 
+        const { post_comment_id, user_id, reply, date_replied } = await request.json();
+        const client = await pool.connect();
+        const result = await client.query("insert into comment_reply (post_comment_id, user_id, reply, date_replied) values ($1, $2, $3, $4)", [post_comment_id, user_id, reply, date_replied]);
+        client.release();
+
+        return new Response(JSON.stringify(result.rows), { status: 200 });
     } catch (error) {
-        return new Response("Failed to retrieve data", {
-            status: 500,
+        return new Response("Failed to retrieve data", { status: 500 });
+    }
+}
+
+export async function PUT(request: NextRequest) {
+    try {
+        const pool = new Pool({
+            user: "postgres",
+            password: "Bingus_LLC",
+            host: "bingus-db-1.c9ayqsiuu3wz.us-east-1.rds.amazonaws.com",
+            port: 5432,
+            database: "bingus",
+            ssl: {
+                rejectUnauthorized: false,
+            },
         });
+
+        const { comment_reply_id, post_comment_id, user_id, reply, date_replied } = await request.json();
+        const client = await pool.connect();
+        const result = await client.query(`update comment_reply set post_comment_id = $2 where comment_reply_id = $1`, [comment_reply_id, post_comment_id, user_id, reply, date_replied]);
+        client.release();
+
+        return new Response(JSON.stringify(result.rows), { status: 200 });
+    } catch (error) {
+        return new Response("Failed to retrieve data", { status: 500 });
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    try {
+        const pool = new Pool({
+            user: "postgres",
+            password: "Bingus_LLC",
+            host: "bingus-db-1.c9ayqsiuu3wz.us-east-1.rds.amazonaws.com",
+            port: 5432,
+            database: "bingus",
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        });
+
+        const { id } = await request.json();
+        const client = await pool.connect();
+        const result = await client.query("delete from comment_reply where comment_reply_id=$1", [id]);
+        client.release();
+
+        return new Response(JSON.stringify(result.rows), { status: 200 });
+    } catch (error) {
+        return new Response("Failed to retrieve data", { status: 500 })
     }
 }
--- bingus/src/components/Content.tsx
+++ bingus/src/components/Content.tsx
@@ -33,23 +33,42 @@ const Content = () => {
         }
     };
 
+    const deleteRow = async (endpoint: string, rowIndex: number) => {
+        try {
+            const row = data[endpoint][rowIndex];
+            const id = Object.values(row)[0];
+
+            const response = await fetch(`http://localhost:3000/api/${endpoint}`, {
+                method: "DELETE",
+                body: JSON.stringify({ id }),
+            });
+        
+        } catch (error) {
+            console.error("Error deleting data:", error);
+        }
+    };
+
     return (
         <div className="content-main">
             {api_endpoints.map((endpoint, index) => (
                 <div className="each-endpoint" key={index}>
-                    <button onClick={() => fetchData(endpoint)}>
+                    <button className="button" onClick={() => fetchData(endpoint)}>
                         Fetch {endpoint}
                     </button>
                     {data[endpoint] && data[endpoint].map((row, rowIndex) => (
                         <div key={rowIndex}>
-                            <div className="data-row" style={{ animationDelay: `${0.25 * rowIndex}s` }}>
+                            <button
+                                onClick={() => deleteRow(endpoint, rowIndex)}
+                                className="data-row"
+                                style={{ animationDelay: `${0.25 * rowIndex}s` }}
+                            >
                                 &#8594;&nbsp;
                                 {Object.entries(row).map(([key, value]) => (
                                     <div key={key}>
                                         <strong>{key}</strong>: {JSON.stringify(value)},&nbsp;&nbsp;&nbsp;
                                     </div>
                                 ))}
-                            </div>
+                            </button>
                         </div>
                     ))}
                 </div>
