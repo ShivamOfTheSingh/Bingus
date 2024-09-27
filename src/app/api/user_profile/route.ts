4d29baed4d9a965495673556e9d957922dca0a7e
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2024-09-27 15:36:03 -0700

    Update route.ts

--- src/app/api/user_profile/route.ts
+++ src/app/api/user_profile/route.ts
@@ -26,7 +26,6 @@ export async function POST(request: Request): Promise<Response> {
     try {
         // Get user profile from HTTP request data
         const userProfile: UserProfile = await request.json();
-        return new Response("DB_HOST: " + process.env.DB_HOST, { status: 500 });
         // Open DB connection
         client = await pool.connect();
         // Check if user already exists
