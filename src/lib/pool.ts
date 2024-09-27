8b2df31fe14178e1b0219bfdb76f3ead3dce3bd1
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2024-09-27 15:17:55 -0700

    Update pool.ts

--- src/lib/pool.ts
+++ src/lib/pool.ts
@@ -6,7 +6,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+    port: 5432,
     database: process.env.DB_NAME,
     ssl: {
         rejectUnauthorized: false,
