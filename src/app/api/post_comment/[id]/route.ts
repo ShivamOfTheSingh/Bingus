5028dd0f503f7ce5b7ef4d3ccba1963ea8bffa3d
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-06 17:53:56 -0700

    update API for post_comment

--- src/app/api/post_comment/[id]/route.ts
+++ src/app/api/post_comment/[id]/route.ts
@@ -1,16 +1,39 @@
-// api/post_comment/[id]
-import { NextRequest } from "next/server";
+import { PostComment } from "@/lib/models";
 import pool from "../../../../lib/pool";
 
-export async function GET(request: NextRequest, { params }: { params: { id: number } }) {
+/**
+ * GET endpoint for table post_comment - single row by id
+ * 
+ * @param {Request} request The incoming HTTP request
+ * @param {string} param1 The id of the row to return
+ * @returns {Response} Status code HTTP response
+ */
+export async function GET(request: Request, { params }: { params: { id: string } }): Promise<Response> {
+    let client;
     try {
-        const { id } = params
-        const client = await pool.connect();
-        const result = await client.query("select * from post_comment where post_comment_id = $1", [id]);
-        client.release();
+        const id = parseInt(params.id);
+        client = await pool.connect();
+        const result = await client.query("SELECT * FROM post_comment WHERE post_comment_id = $1", [id]);
 
-        return new Response(JSON.stringify(result.rows), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to delete data", { status: 500 });
+        if (result.rows.length === 0) {
+            return new Response("Post comment record not found", { status: 404 });
+        }
+
+        const postComment: PostComment = {
+            postCommentId: result.rows[0].post_comment_id,
+            postId: result.rows[0].post_id,
+            userId: result.rows[0].user_id,
+            postComment: result.rows[0].post_comment,
+            dateCommented: new Date(result.rows[0].date_commented)  // Convert to Date object
+        };
+        return new Response(JSON.stringify(postComment), { status: 200 });
+    } 
+    catch (error) {
+        return new Response("Failed to retrieve data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
--- src/app/api/post_comment/route.ts
+++ src/app/api/post_comment/route.ts
@@ -1,60 +1,114 @@
-// api/post_comment
-import { NextRequest } from "next/server";
+import { PostComment } from "@/lib/models";
 import pool from "../../../lib/pool";
 
-export async function GET(request: NextRequest) {
+/**
+ * GET endpoint for table post_comment (Fetch all post comments)
+ * 
+ * @param {Request} request The incoming HTTP request
+ * @returns {Response} The HTTP response containing an array of PostComment objects or an error code
+ */
+export async function GET(request: Request): Promise<Response> {
+    let client;
     try {
-        const client = await pool.connect();
+        client = await pool.connect();
         const result = await client.query("SELECT * FROM post_comment");
-        client.release();
-
-        return new Response(JSON.stringify(result.rows), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to retrieve data", { status: 500 });
+        const postComments: PostComment[] = result.rows.map((row: any) => (
+            {
+                postCommentId: row.post_comment_id,
+                postId: row.post_id,
+                userId: row.user_id,
+                postComment: row.post_comment,
+                dateCommented: new Date(row.date_commented),  // Convert to Date object
+            }
+        ));
+        return new Response(JSON.stringify(postComments), { status: 200 });
+    }
+    catch (error) {
+        return new Response("Failed to fetch data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
 
-export async function POST(request: NextRequest) {
+/**
+ * POST endpoint for table post_comment (Create a new post comment)
+ * 
+ * @param {Request} request The incoming HTTP request with PostComment object as the body
+ * @returns {Response} Status code HTTP response
+ */
+export async function POST(request: Request): Promise<Response> {
+    let client;
     try {
-        const { post_id, user_id, post_comment, date_commented } = await request.json();
-        const client = await pool.connect();
-        const result = await client.query(
-            "INSERT INTO post_comment (post_id, user_id, post_comment, date_commented) VALUES ($1, $2, $3, $4) RETURNING *",
-            [post_id, user_id, post_comment, date_commented]
+        const postComment: PostComment = await request.json();
+        client = await pool.connect();
+        await client.query(
+            "INSERT INTO post_comment (post_id, user_id, post_comment, date_commented) VALUES ($1, $2, $3, $4)", 
+            [postComment.postId, postComment.userId, postComment.postComment, postComment.dateCommented]
         );
-        client.release();
-
-        return new Response(JSON.stringify(result.rows[0]), { status: 201 });
-    } catch (error) {
+        return new Response("OK", { status: 201 });
+    } 
+    catch (error) {
         return new Response("Failed to create data", { status: 500 });
     }
-}
-
-export async function PUT(request: NextRequest) {
-    try {
-        const { post_comment_id, post_id, user_id, post_comment, date_commented } = await request.json();
-        const client = await pool.connect();
-        const result = await client.query(
-            "UPDATE post_comment SET post_id = $2, user_id = $3, post_comment = $4, date_commented = $5 WHERE post_comment_id = $1 RETURNING *",
-            [post_comment_id, post_id, user_id, post_comment, date_commented]
-        );
-        client.release();
-
-        return new Response(JSON.stringify(result.rows[0]), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to update data", { status: 500 });
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
 
-export async function DELETE(request: NextRequest) {
+/**
+ * PUT endpoint for table post_comment (Update an existing post comment)
+ * 
+ * @param {Request} request The incoming HTTP request with PostComment object as the body
+ * @returns {Response} Status code HTTP response
+ */
+export async function PUT(request: Request): Promise<Response> {
+    let client;
+    try {
+        const postComment: PostComment = await request.json();
+        client = await pool.connect();
+        await client.query(
+            "UPDATE post_comment SET post_id = $2, user_id = $3, post_comment = $4, date_commented = $5 WHERE post_comment_id = $1", 
+            [postComment.postCommentId, postComment.postId, postComment.userId, postComment.postComment, postComment.dateCommented]
+        );
+
+        return new Response("OK", { status: 200 });
+    } 
+    catch (error) {
+        return new Response("Failed to update data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
+    }
+}
+
+/**
+ * DELETE endpoint for table post_comment (Delete a post comment by ID)
+ * 
+ * @param {Request} request The incoming HTTP request containing the ID of the post comment to delete
+ * @returns {Response} Status code HTTP response
+ */
+export async function DELETE(request: Request): Promise<Response> {
+    let client;
     try {
         const { id } = await request.json();
-        const client = await pool.connect();
-        const result = await client.query("DELETE FROM post_comment WHERE post_comment_id = $1", [id]);
-        client.release();
+        client = await pool.connect();
+        await client.query("DELETE FROM post_comment WHERE post_comment_id = $1", [id]);
 
-        return new Response(JSON.stringify(result.rows), { status: 200 });
-    } catch (error) {
+        return new Response("OK", { status: 200 });
+    } 
+    catch (error) {
         return new Response("Failed to delete data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
