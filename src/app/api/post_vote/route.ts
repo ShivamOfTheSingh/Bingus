0ee8429a61f26a55c27d40a389567aceb5a30f6e
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-06 17:56:42 -0700

    update API for post_vote

--- src/app/api/post_vote/route.ts
+++ src/app/api/post_vote/route.ts
@@ -1,60 +1,113 @@
-// api/post_vote
-import { NextRequest } from "next/server";
+import { PostVote } from "@/lib/models";
 import pool from "../../../lib/pool";
 
-export async function GET(request: NextRequest) {
+/**
+ * GET endpoint for table post_vote (Fetch all post votes)
+ * 
+ * @param {Request} request The incoming HTTP request
+ * @returns {Response} The HTTP response containing an array of PostVote objects or an error code
+ */
+export async function GET(request: Request): Promise<Response> {
+    let client;
     try {
-        const client = await pool.connect();
+        client = await pool.connect();
         const result = await client.query("SELECT * FROM post_vote");
-        client.release();
-
-        return new Response(JSON.stringify(result.rows), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to retrieve data", { status: 500 });
+        const postVotes: PostVote[] = result.rows.map((row: any) => (
+            {
+                postVoteId: row.post_likes_id,
+                postId: row.post_id,
+                userId: row.user_id,
+                postVoteValue: row.post_vote_value
+            }
+        ));
+        return new Response(JSON.stringify(postVotes), { status: 200 });
+    }
+    catch (error) {
+        return new Response("Failed to fetch data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
 
-export async function POST(request: NextRequest) {
+/**
+ * POST endpoint for table post_vote (Create a new post vote)
+ * 
+ * @param {Request} request The incoming HTTP request with PostVote object as the body
+ * @returns {Response} Status code HTTP response
+ */
+export async function POST(request: Request): Promise<Response> {
+    let client;
     try {
-        const { post_id, user_id, vote_value } = await request.json();
-        const client = await pool.connect();
-        const result = await client.query(
-            "INSERT INTO post_vote (post_id, user_id, vote_value) VALUES ($1, $2, $3) RETURNING *",
-            [post_id, user_id, vote_value]
+        const postVote: PostVote = await request.json();
+        client = await pool.connect();
+        await client.query(
+            "INSERT INTO post_vote (post_id, user_id, post_vote_value) VALUES ($1, $2, $3)", 
+            [postVote.postId, postVote.userId, postVote.postVoteValue]
         );
-        client.release();
-
-        return new Response(JSON.stringify(result.rows[0]), { status: 201 });
-    } catch (error) {
+        return new Response("OK", { status: 201 });
+    } 
+    catch (error) {
         return new Response("Failed to create data", { status: 500 });
     }
-}
-
-export async function PUT(request: NextRequest) {
-    try {
-        const { post_vote_id, post_id, user_id, vote_value } = await request.json();
-        const client = await pool.connect();
-        const result = await client.query(
-            "UPDATE post_vote SET post_id = $2, user_id = $3, vote_value = $4 WHERE post_vote_id = $1 RETURNING *",
-            [post_vote_id, post_id, user_id, vote_value]
-        );
-        client.release();
-
-        return new Response(JSON.stringify(result.rows[0]), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to update data", { status: 500 });
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
 
-export async function DELETE(request: NextRequest) {
+/**
+ * PUT endpoint for table post_vote (Update an existing post vote)
+ * 
+ * @param {Request} request The incoming HTTP request with PostVote object as the body
+ * @returns {Response} Status code HTTP response
+ */
+export async function PUT(request: Request): Promise<Response> {
+    let client;
+    try {
+        const postVote: PostVote = await request.json();
+        client = await pool.connect();
+        await client.query(
+            "UPDATE post_vote SET post_id = $2, user_id = $3, post_vote_value = $4 WHERE post_likes_id = $1", 
+            [postVote.postVoteId, postVote.postId, postVote.userId, postVote.postVoteValue]
+        );
+
+        return new Response("OK", { status: 200 });
+    } 
+    catch (error) {
+        return new Response("Failed to update data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
+    }
+}
+
+/**
+ * DELETE endpoint for table post_vote (Delete a post vote by ID)
+ * 
+ * @param {Request} request The incoming HTTP request containing the ID of the post vote to delete
+ * @returns {Response} Status code HTTP response
+ */
+export async function DELETE(request: Request): Promise<Response> {
+    let client;
     try {
         const { id } = await request.json();
-        const client = await pool.connect();
-        const result = await client.query("DELETE FROM post_vote WHERE post_vote_id = $1", [id]);
-        client.release();
+        client = await pool.connect();
+        await client.query("DELETE FROM post_vote WHERE post_likes_id = $1", [id]);
 
-        return new Response(JSON.stringify({ message: "Vote deleted successfully" }), { status: 200 });
-    } catch (error) {
+        return new Response("OK", { status: 200 });
+    } 
+    catch (error) {
         return new Response("Failed to delete data", { status: 500 });
+    }
+    finally {
+        if (client) {
+            client.release();
+        }
     }
 }
