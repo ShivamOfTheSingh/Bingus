307fb20a54c849843f7999d2e56a747612a0068c
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2024-10-07 20:22:38 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      NEXT_PUBLIC_DB_HOST: process.env.DB_HOST,
-      NEXT_PUBLIC_DB_NAME: process.env.DB_NAME,
-      NEXT_PUBLIC_DB_PASSWORD: process.env.DB_PASSWORD,
-      NEXT_PUBLIC_DB_USER: process.env.DB_USER,
-      NEXT_PUBLIC_SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY
+      DB_HOST: process.env.DB_HOST,
+      DB_NAME: process.env.DB_NAME,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_USER: process.env.DB_USER,
+      SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY
     }
   };
   
