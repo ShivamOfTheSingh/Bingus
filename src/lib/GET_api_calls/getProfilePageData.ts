8fc65b7c9ccad4aa207e45095d6d003991218ef5
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-21 01:15:56 -0700

    daquavious gooch

--- src/lib/GET_api_calls/getProfilePageData.ts
+++ src/lib/GET_api_calls/getProfilePageData.ts
@@ -17,16 +17,16 @@ interface ReturnData {
  *                       and an array of objects that each contain a post and an array of that post's media.
  */
 export default async function getProfilePageData(userId: number): Promise<ReturnData> {
-    const resProfile = await fetch(`http://localhost:3000/api/crud/user_profile/${userId}`);
+    const resProfile = await fetch(`https://production.d3drl1bcjmxovs.amplifyapp.com/api/crud/user_profile/${userId}`);
     if (resProfile.status === 404) notFound();
     const profile: UserProfile = await resProfile.json();
 
-    const resPosts = await fetch(`http://localhost:3000/api/crud/user_profile/posts/${userId}`);
+    const resPosts = await fetch(`https://production.d3drl1bcjmxovs.amplifyapp.com/api/crud/user_profile/posts/${userId}`);
     const posts: Post[] = await resPosts.json();
 
     const postsWithMedia: { post: Post, media: Media[] }[] = [];
     for (let i = 0; i < posts.length; i++) {
-      const resMedia = await fetch(`http://localhost:3000/api/crud/posts/media/${posts[i].postId}`);
+      const resMedia = await fetch(`https://production.d3drl1bcjmxovs.amplifyapp.com/api/crud/posts/media/${posts[i].postId}`);
       const mediaArray: Media[] = await resMedia.json();
       postsWithMedia.push({
         post: posts[i],
@@ -34,10 +34,10 @@ export default async function getProfilePageData(userId: number): Promise<Return
       });
     }
 
-    const resFollowers = await fetch(`http://localhost:3000/api/crud/followings/numFollowers/${userId}`);
+    const resFollowers = await fetch(`https://production.d3drl1bcjmxovs.amplifyapp.com/api/crud/followings/numFollowers/${userId}`);
     const numFollowers = await resFollowers.json();
 
-    const resFollowing = await fetch(`http://localhost:3000/api/crud/followings/numFollowing/${userId}`);
+    const resFollowing = await fetch(`https://production.d3drl1bcjmxovs.amplifyapp.com/api/crud/followings/numFollowing/${userId}`);
     const numFollowing = await resFollowing.json();
 
     return {
