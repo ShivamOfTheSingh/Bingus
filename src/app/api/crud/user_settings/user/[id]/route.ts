d1afe3f5f31756cc1999d2b9385ff3e723c83433
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-26 15:32:56 -0700

    Add API route to get user settings by USER ID

--- src/app/api/crud/user_settings/user/[id]/route.ts
+++ src/app/api/crud/user_settings/user/[id]/route.ts
@@ -1,22 +1,14 @@
+import pool from "@/lib/db/pool";
 import { UserSettings } from "@/lib/db/models";
-import pool from "../../../../../lib/db/pool";
 
-/**
- * GET endpoint for table user_settings - single row by id
- * 
- * @param {Request} request The incoming HTTP request
- * @param {string} param1 The id of the row to return
- * @returns {Response} Status code HTTP response
- */
 export async function GET(request: Request, { params }: { params: { id: string } }): Promise<Response> {
     let client;
     try {
-        const id = parseInt(params.id);
         client = await pool.connect();
-        const result = await client.query("SELECT * FROM user_settings WHERE user_settings_id = $1", [id]);
+        const result = await client.query("SELECT * FROM user_settings WHERE user_id = $1", [params.id]);
 
         if (result.rows.length === 0) {
-            return new Response("User settings record not found", { status: 404 });
+            return new Response("User settings not found", { status: 404 });
         }
 
         const userSettings: UserSettings = {
@@ -26,10 +18,11 @@ export async function GET(request: Request, { params }: { params: { id: string }
             profilePublic: result.rows[0].profile_public
         };
         return new Response(JSON.stringify(userSettings), { status: 200 });
-    } 
-    catch (error) {
-        return new Response("Failed to retrieve data", { status: 500 });
-    } 
+    }
+    catch (error: any) {
+        console.log(error);
+        return new Response("Internal Server Error", { status: 500 });
+    }
     finally {
         if (client) {
             client.release();
