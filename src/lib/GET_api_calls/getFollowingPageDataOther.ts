030c1432c55accca13de1002cd08c6ea19949beb
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-26 22:06:02 -0700

    change urls to production

--- src/lib/GET_api_calls/getFollowingPageDataOther.ts
+++ src/lib/GET_api_calls/getFollowingPageDataOther.ts
@@ -2,7 +2,7 @@ import { UserProfile } from "../db/models";
 
 export async function getFollowingPageDataOther(userId: number): Promise<UserProfile[]> {
     try {
-        const response = await fetch(`http://localhost:3000/api/crud/user_profile/followings/${userId}`);
+        const response = await fetch(`https://production.d3drl1bcjmxovs.amplifyapp.com/api/crud/user_profile/followings/${userId}`);
         const users: UserProfile[] = await response.json();
         return users;
     }
