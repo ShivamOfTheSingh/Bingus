83ab6f7992f1f9a5e1684337eea1a380854800a1
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-27 17:41:55 -0700

    bingus

--- src/lib/form_schemas/profileSettingsFormSchema.ts
+++ src/lib/form_schemas/profileSettingsFormSchema.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 const profileSettingsFormSchema = z.object({
-    profilePicture: z.object({
+    profilePicture: z.optional(z.object({
         name: z.string(),
         size: z.number(),
         type: z.string().refine(
             (type) => ["image/jpeg", "image/png", "video/mp4", "image/gif"].includes(type),
             { message: "Only JPG, PNG, GIF, and MP4 formats are allowed" }
             ),
-        }),
+        })),
     gender: z
         .enum(["male", "female", "other", "prefer_not_to_say"], {
           errorMap: (issue, ctx) => ({ message: 'Gender is required' })
