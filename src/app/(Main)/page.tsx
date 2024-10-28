b3fe8595460911805760c6c4b718ed5cf755a9a6
damiankryzia71 <damiankryzia71@gmail.com>
2024-10-27 17:13:29 -0700

    bingus

--- src/app/(Main)/page.tsx
+++ src/app/(Main)/page.tsx
@@ -1,4 +1,14 @@
-export default function Page() {
+"use server";
+
+import getCurrentSessionUserId from "@/lib/cookies/getCurrentSessionUserId";
+import SessionInactive from "@/components/SessionInactive";
+
+export default async function Page() {
+    const userId = await getCurrentSessionUserId();
+    if (userId === -1) {
+        return <SessionInactive />;
+    }
+
     return (
         <div className="flex justify-center">
             <div>This is the main page</div>
