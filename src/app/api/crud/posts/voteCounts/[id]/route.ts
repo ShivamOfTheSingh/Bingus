d0cff542c35a3b73e3a60e81a4d8b2e1056f6f55
damiankryzia71 <damiankryzia71@gmail.com>
2024-11-03 14:07:48 -0800

    lieks bug fix

--- src/app/api/crud/posts/voteCounts/[id]/route.ts
+++ src/app/api/crud/posts/voteCounts/[id]/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request, { params }: { params: { id: string }
         const result = await client.query("SELECT COUNT(*) FROM post_vote WHERE post_id = $1", [params.id]);
 
         const response = {
-            count: result.rows[0].count
+            count: parseInt(result.rows[0].count)
         };
 
         return new Response(JSON.stringify(response), { status: 200 });
