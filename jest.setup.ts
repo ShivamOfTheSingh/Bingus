c748994efc1e24a90dbec4f2825dc08f3d518321
damiankryzia71 <damiankryzia71@gmail.com>
2024-11-12 22:10:04 -0800

    setup some unit tests

---
+++ jest.setup.ts
@@ -0,0 +1,8 @@
+import { TextEncoder, TextDecoder } from 'util';
+import fetch, { Request, Response } from 'cross-fetch';
+
+global.TextEncoder = TextEncoder as any;
+global.TextDecoder = TextDecoder as any;
+global.fetch = fetch as any;
+global.Request = Request as any;
+global.Response = Response as any;
